581f8c53145a3cf5eb9458a957c33e19be8d47f4
Gustavo <57189396+GuzGg@users.noreply.github.com>
2025-08-25 23:00:54 +0100

    virtual anchor thread

---
+++ synchronizer/src/main/java/uwb/simulator/VirtualAnchorThread.java
@@ -0,0 +1,20 @@
+package uwb.simulator;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VirtualAnchorThread extends Thread {
+	private VirtualAnchor anchor;
+	private static final AtomicInteger count = new AtomicInteger(0); 
+	private int anchorID = 0;
+	
+	private VirtualAnchorThread () {
+		this.anchor = new VirtualAnchor("anchor"+anchorID, LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli(), LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+		this.anchorID = count.incrementAndGet(); 
+	}
+	
+	public void run() {
+		
+	}
+}
